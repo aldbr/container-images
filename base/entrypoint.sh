cbe9eb8b051a503792d80ce9ef4759487bc5b1a8
Chris Burr <christopher.burr@cern.ch>
2023-11-28 02:50:03 +0100

    Install all entrypoint packages in a single pip install command

--- base/entrypoint.sh
+++ base/entrypoint.sh
@@ -6,6 +6,7 @@ micromamba activate base
 
 function install_sources() {
     IFS=','
+    to_install=()
     for dir in ${DIRACX_CUSTOM_SOURCE_PREFIXES}; do
         for package_name in ${DIRACX_IMAGE_PACKAGES}; do
             if [[ "${package_name}" == "." ]]; then
@@ -18,16 +19,18 @@ function install_sources() {
                 echo "ERROR: Multiple wheels found for ${package_name} in ${dir}"
                 exit 1
             elif [[ ${#wheels[@]} -eq 1 ]]; then
-                pip install --no-deps "${wheels[0]}"
+                to_install+=("${wheels[0]}")
             elif [[ -d "${dir}-${package_name}" ]] || [[ "${package_name}" == "." ]]; then
                 if [[ -n "${DIRACX_CUSTOM_SOURCE_EDITABLE:-}" ]]; then
-                    pip install --no-deps -e "${dir}-${package_name}"
-                else
-                    pip install --no-deps "${dir}-${package_name}"
+                    to_install+=("-e")
                 fi
+                to_install+=("${dir}-${package_name}")
             fi
         done
     done
+    if [[ ${#to_install[@]} -gt 0 ]]; then
+        pip install --no-deps "${to_install[@]}"
+    fi
 }
 
 if [[ -n "${DIRACX_CUSTOM_SOURCE_PREFIXES:-}" ]]; then
